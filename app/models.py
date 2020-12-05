9a25d06065d75be2fbbc271e79b41732b7beb2ae
Bella Amandine <bellamandy45@gmail.com>
2020-12-05 13:04:14 +0200

    Define relationship between classes

--- app/models.py
+++ app/models.py
@@ -14,6 +14,7 @@ class Writer(db.Model):
     bio = db.Column(db.String(255))
     profile_pic_path = db.Column(db.String(255))
     pass_secure = db.Column(db.String(255))
+    blogs = db.relationship('Blog', backref = 'writer', lazy = "dynamic")
 
     def __repr__(self):
         return f'User {self.email}'
@@ -29,6 +30,9 @@ class Blog(db.Model):
     blog_message = db.Column(db.String(255))
     blog_pic_path = db.Column(db.String(255))
     posted_date = db.Column(db.DateTime, default=datetime.utcnow)
+    writer_id = db.Column(db.Integer, db.ForeignKey('writers.id'))
+    comments = db.relationship('Comment', backref = 'blog', lazy = "dynamic")
+
 
     def __repr__(self):
         return f'User {self.blog_message}'
@@ -43,6 +47,7 @@ class Comment(db.Model):
     id = db.Column(db.Integer, primary_key = True)
     comment_message = db.Column(db.String(255))
     published_at = db.Column(db.DateTime, default=datetime.utcnow)
+    blog_id = db.Column(db.Integer, db.ForeignKey('blogs.id'))
 
     def __repr__(self):
         return f'User {self.comment_message}'
