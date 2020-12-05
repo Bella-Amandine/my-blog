87e14b20d37709bb5f9dc37bf39b8b1b32bb4249
Bella Amandine <bellamandy45@gmail.com>
2020-12-05 11:51:22 +0200

    Initialize flask application and add some configurations

---
+++ config.py
@@ -0,0 +1,30 @@
+class Config:
+    '''
+    General configuration parent class
+    '''
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://amandine:pass@localhost/blog'
+    pass
+
+class DevConfig(Config):
+    '''
+    Development configuration child class
+
+    Args:
+        Config: The parent configuration class with general configuration settings
+    '''
+
+    DEBUG = True
+
+class ProdConfig(Config):
+    '''
+    Production configuration child class
+
+    Args: 
+        Config: The parent configuration class with General configuration settings
+    '''
+    pass
+
+config_options = {
+    'development' : DevConfig(),
+    'production' : ProdConfig()
+}
