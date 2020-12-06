2ce32fe168e948f532a1ccbdec5bf7b54cebafe3
Bella Amandine <bellamandy45@gmail.com>
2020-12-06 15:09:58 +0200

    implement authentication system

--- app/models.py
+++ app/models.py
@@ -1,8 +1,15 @@
 from . import db
 from datetime import datetime
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import UserMixin
+from . import login_manager
+
+@login_manager.user_loader
+def load_user(writer_id):
+    return Writer.query.get(int(writer_id))
 
 
-class Writer(db.Model):
+class Writer(UserMixin, db.Model):
     '''
     Writer class to create a writer model
     '''
@@ -16,6 +23,17 @@ class Writer(db.Model):
     profile_pic_path = db.Column(db.String(255))
     pass_secure = db.Column(db.String(255))
     blogs = db.relationship('Blog', backref = 'writer', lazy = "dynamic")
+
+    @property
+    def password(self):
+        raise AttributeError("You cannot read the password attribute")
+
+    @password.setter
+    def password(self, password):
+        self.pass_secure = generate_password_hash(password)
+
+    def verify_password(self, password):
+        return check_password_hash(self.pass_secure, password)
 
     def __repr__(self):
         return f'User {self.email}'
